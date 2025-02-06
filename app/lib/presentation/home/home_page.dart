3690aa104011a2e11af7e38a824e7147bdc55561
Jonas <jonas@firedev.tech>
2025-02-06 22:18:07 +0100

    add creadits and url launcher

--- app/lib/presentation/home/home_page.dart
+++ app/lib/presentation/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,19 +18,50 @@ class HomePage extends StatelessWidget {
     );
     return Scaffold(
       appBar: AppBar(title: Text('FD Toolkit')),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(20),
+      body: Column(
         children: [
-          _buildToolCard(
-            context,
-            'LinkedIn Carousel',
-            '/social-media/linkedin-carousel',
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              padding: EdgeInsets.all(20),
+              children: [
+                _buildToolCard(
+                  context,
+                  'LinkedIn Carousel',
+                  '/social-media/linkedin-carousel',
+                ),
+                _buildToolCard(
+                  context,
+                  'Compress Images',
+                  '/converter/image-compressor',
+                ),
+              ],
+            ),
           ),
-          _buildToolCard(
-            context,
-            'Compress Images',
-            '/converter/image-compressor',
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Collection of tools',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  TextSpan(
+                    text: '   |   by ',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  TextSpan(
+                    text: 'jojo_firedev',
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () =>
+                          launchUrlString('https://github.com/jojo-firedev'),
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
@@ -41,11 +74,14 @@ class HomePage extends StatelessWidget {
       child: Card(
         margin: EdgeInsets.all(10),
         elevation: 5,
-        child: Center(
-          child: Text(
-            title,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: 100, maxWidth: 200),
+          child: Center(
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
           ),
         ),
       ),
